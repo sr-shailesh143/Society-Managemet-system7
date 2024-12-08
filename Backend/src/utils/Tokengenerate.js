6ab8611d8fb28afcc26d9897b97316dc64e88bad
sr-shailesh143 <shailesh.patel@swiftrut.com>
2024-12-08 15:57:47 +0530

    commit

--- Backend/src/utils/Tokengenerate.js
+++ Backend/src/utils/Tokengenerate.js
@@ -15,7 +15,7 @@ exports.generateToeken = (userId, res) => {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", 
-        secure: process.env.NODE_ENV !== "development"
+        secure: process.env.NODE_ENV !== "production"
     });
 
     return token;
