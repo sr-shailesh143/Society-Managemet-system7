5103f358d8f07876386b8b99610d9e0d0f1ffa08
sr-shailesh143 <shailesh.patel@swiftrut.com>
2024-12-09 03:10:31 +0530

    Update Authentication.jsx

--- Frontend/src/apiservices/Authentication.jsx
+++ Frontend/src/apiservices/Authentication.jsx
@@ -13,7 +13,7 @@ export const signup = async (data) =>
   await api.post("http://localhost:8001/api/auth/signup", data);
 
 // Login
-export const login = async (data) => await api.post("https://society-managemet-system7-87o7.onrender.com/api/auth/login", data);
+export const login = async (data) => await api.post("api/auth/login", data);
 
 // Logout
 export const logout = async () => await api.post("https://society-managemet-system7-87o7.onrender.com/api/auth/logout");
